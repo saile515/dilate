4be5a177a805183191d35e1fb8e3b685bebcf338
saile515 <elias.jorgensen2006@gmail.com>
2023-04-17 22:37:15 +0200

    Added speedometer graphics

--- src/game/main.ts
+++ src/game/main.ts
@@ -9,71 +9,79 @@ import Vector2 from "../engine/core/Vector2";
 import { keyMap } from "../engine/input/keyboard";
 
 declare global {
-	var scene: Scene;
+    var scene: Scene;
 }
 
 export function init() {
-	const canvas: HTMLCanvasElement = document.querySelector("#game_canvas");
-	const ctx = canvas.getContext("2d");
+    const canvas: HTMLCanvasElement = document.querySelector("#game_canvas");
+    const ctx = canvas.getContext("2d");
 
-	document.documentElement.ontouchstart = () => {
-		document.documentElement.requestFullscreen();
-		screen.orientation.lock("landscape");
-	};
+    document.documentElement.ontouchstart = () => {
+        document.documentElement.requestFullscreen();
+        screen.orientation.lock("landscape");
+    };
 
-	const scene = new Scene(ctx);
-	globalThis.scene = scene;
+    const scene = new Scene(ctx);
+    globalThis.scene = scene;
 
-	const player = new Object();
-	const playerSprite = new Sprite("/ship.png");
-	player.addComponent(playerSprite);
-	const rigidbody = new Rigidbody();
-	rigidbody.mass = 10 * 10 ** 6;
-	player.addComponent(rigidbody);
-	scene.add(player);
+    const player = new Object();
+    const playerSprite = new Sprite("/ship.png");
+    player.addComponent(playerSprite);
+    const rigidbody = new Rigidbody();
+    rigidbody.mass = 10 * 10 ** 6;
+    player.addComponent(rigidbody);
+    scene.add(player);
 
-	scene.inputHandler.addAction(new InputAction<boolean>("accelerate"));
-	keyMap.set("w", "accelerate");
+    scene.inputHandler.addAction(new InputAction<boolean>("accelerate"));
+    keyMap.set("w", "accelerate");
 
-	scene.inputHandler.addAction(new InputAction<boolean>("decelerate"));
-	keyMap.set("s", "decelerate");
+    scene.inputHandler.addAction(new InputAction<boolean>("decelerate"));
+    keyMap.set("s", "decelerate");
 
-	const playerTransform = player.getComponent<Transform>(Transform);
+    const playerTransform = player.getComponent<Transform>(Transform);
 
-	function update() {
-		scene.inputHandler.update();
+    const c = 299792458;
+    let gammaFactor = 1;
 
-		if (scene.inputHandler.getAction("accelerate").value && rigidbody.velocity.y < 300000) {
-			rigidbody.addForce(new Vector2(0, 10 ** 4));
-		} else {
-			rigidbody.addForce(new Vector2(0, -rigidbody.velocity.y / 2));
-		}
+    let timer = 65;
 
-		if (scene.inputHandler.getAction("decelerate").value && rigidbody.velocity.y < 300000) {
-			rigidbody.addForce(new Vector2(0, -(10 ** 4)));
-		}
+    function update() {
+        scene.inputHandler.update();
 
-		scene.cameraPosition.set(
-			playerTransform.position.x +
-				(rigidbody.velocity.x > 0
-					? Math.min(Math.sqrt(Math.abs(rigidbody.velocity.x)) * 0.005, 1)
-					: Math.max(Math.sqrt(Math.abs(rigidbody.velocity.x)) * 0.005, -1)),
-			playerTransform.position.y +
-				(rigidbody.velocity.y > 0
-					? Math.min(Math.sqrt(Math.abs(rigidbody.velocity.y)) * 0.005, 1)
-					: -Math.max(Math.sqrt(Math.abs(rigidbody.velocity.y)) * 0.005, -1))
-		);
+        if (scene.inputHandler.getAction("accelerate").value && rigidbody.velocity.y < c) {
+            rigidbody.addForce(new Vector2(0, 10 ** 7));
+        } else {
+            rigidbody.addForce(new Vector2(0, -rigidbody.velocity.y / 2));
+        }
 
-		scene.cameraPosition.add(
-			(Math.random() * rigidbody.velocity.magnitude()) / 10000000,
-			(Math.random() * rigidbody.velocity.magnitude()) / 10000000
-		);
+        if (scene.inputHandler.getAction("decelerate").value && rigidbody.velocity.y < c) {
+            rigidbody.addForce(new Vector2(0, -(10 ** 7)));
+        }
 
-		UIStateStore.setVelocity(rigidbody.velocity);
+        gammaFactor = 1 / Math.sqrt(1 - (rigidbody.velocity.magnitude() / c) ** 2);
+        if (gammaFactor && scene.deltaTime) {
+            timer -= scene.deltaTime / gammaFactor;
+        }
+        UIStateStore.setTimer(timer);
 
-		scene.render();
-		requestAnimationFrame(update);
-	}
+        scene.cameraPosition.set(
+            0,
+            playerTransform.position.y +
+            (rigidbody.velocity.y > 0
+                ? Math.min(Math.sqrt(Math.abs(rigidbody.velocity.y)) * 0.0001, 1)
+                : Math.max(Math.sqrt(Math.abs(rigidbody.velocity.y)) * 0.0001, -1))
+        );
 
-	update();
+        scene.cameraPosition.add(
+            ((Math.random() - 0.5) * rigidbody.velocity.magnitude()) / 10000000000,
+            ((Math.random() - 0.5) * rigidbody.velocity.magnitude()) / 10000000000
+        );
+
+        UIStateStore.setVelocity(rigidbody.velocity);
+
+        scene.render();
+        requestAnimationFrame(update);
+    }
+
+    update();
 }
--- src/game/uiState.ts
+++ src/game/uiState.ts
@@ -1,31 +1,37 @@
 import Vector2 from "../engine/core/Vector2";
 
 let UIState = {
-	velocity: new Vector2(0, 0),
+    velocity: new Vector2(0, 0),
+    timer: 65,
 };
 
 let uiListeners: any[] = [];
 
 export const UIStateStore = {
-	setVelocity(velocity: Vector2) {
-		UIState = { ...UIState, velocity: velocity };
-		emitChange();
-	},
+    setVelocity(velocity: Vector2) {
+        UIState = { ...UIState, velocity: velocity };
+        emitChange();
+    },
 
-	subscribe(listener: any) {
-		uiListeners = [...uiListeners, listener];
-		return () => {
-			uiListeners = uiListeners.filter((l) => l !== listener);
-		};
-	},
+    setTimer(time: number) {
+        UIState = { ...UIState, timer: time };
+        emitChange();
+    },
 
-	getSnapshot() {
-		return UIState;
-	},
+    subscribe(listener: any) {
+        uiListeners = [...uiListeners, listener];
+        return () => {
+            uiListeners = uiListeners.filter((l) => l !== listener);
+        };
+    },
+
+    getSnapshot() {
+        return UIState;
+    },
 };
 
 function emitChange() {
-	for (let listener of uiListeners) {
-		listener();
-	}
+    for (let listener of uiListeners) {
+        listener();
+    }
 }
